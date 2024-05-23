0de4c207a232ece9da065612c93eb8af6c216704
VKTRenokh <viktorenokh@gmail.com>
2024-05-23 18:27:31 +0300

    feat: add fennel

---
+++ fnl/pipka/init.fnl
@@ -0,0 +1,10 @@
+(fn open []
+  (let [bufnr (vim.api.nvim_create_buf false true)
+        total-width (math.floor (/ vim.o.columns 2.5))]
+    (vim.api.nvim_open_win bufnr false {:width total-width
+                                        :height 12
+                                        :split :left})))
+
+(open)
+
+{:open open}
--- lua/pipka/init.lua
+++ lua/pipka/init.lua
@@ -1,15 +1,7 @@
-local M = {}
-
-function M.open_float()
+local function open()
   local bufnr = vim.api.nvim_create_buf(false, true)
-
-  local total_width = math.floor(vim.o.columns / 2.5)
-
-  local winid = vim.api.nvim_open_win(
-    bufnr,
-    false,
-    { width = total_width, height = 12, split = 'down' }
-  )
+  local total_width = math.floor((vim.o.columns / 2.5))
+  return vim.api.nvim_open_win(bufnr, false, {width = total_width, height = 12, split = "left"})
 end
-
-M.open_float()
+open()
+return {open = open}
